314b01e6398a5664683cc73381caede7dfb1ce29
LonJonn <leon.salsiccia@gmail.com>
2021-06-19 14:58:08 +1000

    pretty git log 😍

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,2 +1,5 @@
 # NVIM
 alias vim="nvim"
+
+# Pretty git log
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --"
