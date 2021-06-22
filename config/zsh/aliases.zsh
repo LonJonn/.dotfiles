b00d278cb0461315aa378060895108f871c9a999
LonJonn <leon.salsiccia@gmail.com>
2021-06-22 10:01:50 +1000

    add ranger

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -3,3 +3,7 @@ alias vim="nvim"
 
 # Pretty git log
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --"
+
+# Ranger
+alias r="ranger"
+alias rr=". ranger"
