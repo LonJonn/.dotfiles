298517d852c0f66cdfdca63ae9b31412b63d284b
LonJonn <leon.salsiccia@gmail.com>
2021-06-24 12:03:35 +1000

    pretty git diff

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,8 +1,9 @@
 # NVIM
 alias vim="nvim"
 
-# Pretty git log
+# Git
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --"
+alias gd="git diff --name-only --diff-filter=d | xargs bat --diff"
 
 # Ranger
 alias r="ranger"
