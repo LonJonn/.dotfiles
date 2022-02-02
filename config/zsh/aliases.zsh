00c138afdc1478319ce60035f07dc0421eac7ddb
LonJonn <leon.salsiccia@gmail.com>
2022-02-02 15:29:30 +1100

    fix git log alias

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -2,7 +2,7 @@
 alias vim="nvim"
 
 # Git
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 alias gd="git diff --name-only --diff-filter=d | xargs bat --diff"
 
 # Ranger
